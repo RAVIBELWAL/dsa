1134fdf6f7d993c23636347456e36dc970245a83
Ravi Belwal <49521600+RAVIBELWAL@users.noreply.github.com>
2021-10-26 20:03:38 +0530

    binarysearch

--- binarysearch/Binarysearch1.java
+++ binarysearch/Binarysearch1.java
@@ -1,5 +1,36 @@
 package dsa.binarysearch;
 
+import java.util.*;
 public class Binarysearch1 {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        
+        int arr[]={1,2,3,4,5,6,7,8,9,10};
+        int n=arr.length;
+        int x=7;
+        int ans=binarySearch(arr,0,n-1,x);
+        
+        if(ans==-1)System.out.print("Element didn't present in the array ");
+        else System.out.print("Element is at index : "+ans);
+        
+    }
     
+    public static int binarySearch(int[] arr,int l,int r,int num)
+    {   
+        if(r>=l)
+        {
+        int mid=(l+r)/2;
+        
+        if(arr[mid]==num)
+         return mid;
+        
+        if(arr[mid]<num)
+        return binarySearch(arr,mid+1,r,num);
+        
+        return binarySearch(arr,l,mid-1,num);
+        
+        }
+        return -1;
+    }
 }
+
