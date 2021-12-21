ab01de4f45f50b010b9fba825dd59019ccf54690
Ravi Belwal <49521600+RAVIBELWAL@users.noreply.github.com>
2021-12-21 18:30:34 +0530

    Create wordDetector.java

---
+++ practice/wordDetector.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+public class wordDetector {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s1 = sc.next();
+        String s2 = sc.next();
+        int count = 0;
+        for(int i=0;i<s2.length();i++)
+        {
+            for(int j=0;j<s1.length();j++)
+            {
+                if(s1.charAt(j)==s2.charAt(i))
+                {
+                    count++;
+                    break;
+                }
+            }
+        }
+        if(count==s2.length())
+        {
+            System.out.println(true);
+        }
+        else
+        {
+            System.out.println(false);
+        }
+    }
+}
+
+/*check if all char of B are in A are same or not
+ abcde
+ abc
+ o/p:true
+ 
+ abcde
+ abf
+ o/p : fasle
+*/
